b89fa336ba66be0bf826aabe9705ce421635e361
Alonso Valdivia <comofuncionaelmundoporalonso@gmail.com>
2021-06-10 22:23:50 -0500

    Eliminado react-hook-form

--- src/components/UploadItem/UploadItem.jsx
+++ src/components/UploadItem/UploadItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Grid from '@material-ui/core/Grid';
 
 import useStyles from './styles';
-import { useForm } from "react-hook-form";
+
 import {Fragment, useState} from 'react';
 
 import { Link, useLocation } from 'react-router-dom';
